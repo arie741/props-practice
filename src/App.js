243ff2cd9ab500442213580c026afd74282fe15e
arie741 <a.pranasakti@gmail.com>
2022-11-28 14:17:07 +0700

    first ocmmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+function Animal(props) {
+  return (
+    <div>
+      <ul>
+        <li>Id: {props._id}</li>
+        <li>Name: {props.name}</li>
+        <li>Age: {props.age}</li>
+        <li>Species: {props.species}</li>
+        <li>Enclosure: {props.enclosure}</li>
+      </ul>
+    </div>
+  );
+}
 
 function App() {
+  const [animalData, setAnimalData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://express-ts-backend.herokuapp.com/zoo")
+      .then(function (response) {
+        // handle success
+        setAnimalData(response.data.animals)
+      })
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {animalData.map((animal, index) => <Animal key={index} id={animal.id} name={animal.name} age={animal.age} species={animal.species} enclosure={animal.enclosure}/>)}
     </div>
   );
 }
